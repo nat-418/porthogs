5503c9156b75ab0234888b30dd3aba303c54c5ad
nat-418 <93013864+nat-418@users.noreply.github.com>
2021-12-01 22:08:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# 🐷 porthogs
+
+Find which processes are hogging what ports!
+
+## Usage
+
+```
+$ porthogs 8080 8000
+    PID TTY          TIME CMD 
+  23580 ?        00:00:23 PM2 v4.4.0: One
+  13675 ?        00:00:48 PM2 v5.1.0: Another
+```
+
+## Installation
+
+Grab a binary for your system in the `dist/` directory:
+
+* FreeBSD x86-64
+* Linux ARM
+* Linux x86
+* Linux x86-64
+* macOS x86-64
---
+++ src/porthogs.tcl
@@ -0,0 +1,62 @@
+#!/usr/bin/env tclsh
+package require Tcl 8.6
+
+set version 0.1.0
+
+proc halt message {
+    puts stderr "Error: $message."
+    exit 1
+}
+
+proc portHog port_number {
+    if {$port_number eq "" || ![string is double $port_number]} {
+        halt "bad port number $port_number."
+    }
+    
+    try {
+        set process_id [exec lsof -ti:$port_number]
+    } on error message {
+        halt "failed to find process on port $port_number"
+    }
+    
+    if {$process_id eq ""} {
+        halt "no process found on port $port_number"
+    }
+    
+    try {
+        set process_info [lindex [split [exec ps -p $process_id] "\n"] end]
+    } on error message {
+        halt "failed to find process name for $process_id"
+    }
+
+    if {$process_info eq ""} {
+        halt "failed to find process name for $process_id"
+    }
+
+    return $process_info
+}
+
+if {$argc eq 0} {
+    halt "no input"
+}
+
+foreach arg $argv {
+    if {$arg in {"" -h -help --help help}} {
+        lappend help "🐷 porthogs v$version"
+        lappend help "Find which processes are hogging what ports!"
+        lappend help {Usage: porthogs [port numbers...]}
+    
+        puts stdout [join $help "\n\n"]
+        exit 0
+    }
+
+    if {$arg in {"" -v -version --version version}} {
+        puts stdout $version
+        exit 0
+    }
+
+    append hogs "\n[portHog $arg]"
+}
+
+puts "    PID TTY          TIME CMD $hogs"
+exit 0
